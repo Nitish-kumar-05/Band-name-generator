2ec54821f99dfaaed2933e137a5efaeb5d01c2dc
nitish kumar <99485371+Nitish-kumar-05@users.noreply.github.com>
2022-02-11 18:50:20 +0530

    Add files via upload

---
+++ Band name generator.py
@@ -0,0 +1,16 @@
+# DAY 1 OF 100 DAYS PYTHON CODE
+
+# Tittle : Band name generator
+
+# Description : Gives you a band name by the input given
+
+print("Welcome to band name generator")
+
+City = input("Which city you were born? \n")
+
+Pet = input("What is your pet name? \n")
+
+print('Your band name will be : ' + City + " " + Pet)
+
+
+# output : Will be the combination of City and Pet respectively
